a61338822e1d64dc9b97618b742a9b3351f279d4
gouttedooo <goudal.donovan@live.fr>
2024-12-03 11:39:27 +0100

    coche remplie pour commentaires

---
+++ jour-01-test.js
@@ -0,0 +1,11 @@
+/**
+ * Additionne deux nombres.
+ * @param {number} a - Le premier nombre.
+ * @param {number} b - Le second nombre.
+ * @returns {number} La somme des deux nombres.
+ */
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(5, 2));
