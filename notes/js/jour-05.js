bcf6d03cae05b682fea833d67f1c50cae7d741a5
gouttedooo <goudal.donovan@live.fr>
2024-12-09 13:24:57 +0100

    exo 8 OK

--- notes/js/jour-05.js
+++ notes/js/jour-05.js
@@ -87,6 +87,7 @@ console.log(countChar("kakkerlak", "k"));
 */
 
 // exo 7
+/*
 const range = (start, end, step = 1) => {
   let newArray = [];
   if (start > end) {
@@ -101,7 +102,15 @@ const range = (start, end, step = 1) => {
   return newArray;
 };
 
-console.log(range(1, 5, 2));
+const rangeUpgraded = (start, end, step = 1) => {
+  let newArray = [];
+  for (let i = start; start > end ? i > end - 1 : i < end + 1; i += step) {
+    newArray.push(i);
+  }
+  return newArray;
+};
+
+console.log(rangeUpgraded(1, 10, 2));
 
 const sum = (array) => {
   let sum = 0;
@@ -112,3 +121,31 @@ const sum = (array) => {
 };
 
 console.log(sum(range(1, 5)));
+*/
+
+// exo 8
+/*
+const reverseArray = (arrayToReverse) => {
+  const newArray = [];
+  for (let i = arrayToReverse.length - 1; i >= 0; i--) {
+    newArray.push(arrayToReverse[i]);
+  }
+  return newArray;
+};
+
+let arrayValue = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(reverseArray(arrayValue));
+
+const reverseArrayInPlace = (arrayToReverse) => {
+  for (let i = 0; i < Math.floor(arrayToReverse.length / 2); i++) {
+    let iPrime = arrayToReverse.length - 1 - i; //i opposé
+    if (iPrime === i) break;
+    let temp = arrayToReverse[i];
+    arrayToReverse[i] = arrayToReverse[iPrime];
+    arrayToReverse[iPrime] = temp;
+  }
+  return arrayToReverse;
+};
+
+console.log(reverseArrayInPlace(arrayValue));
+*/
